d38d66b0656aedfbefce926b90babdd34f067763
Ruhollah Shemirani <Roohy@guest-wireless-207-151-057-250.usc.edu>
2016-10-26 21:13:12 -0700

    fff

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import numpy as np
 from file_readers import *
+from parsers import *
 USER_DATA_ADDR = "./bytecup2016data/user_info.txt"
-
-
+QUESTION_DATA_ADDR = "./bytecup2016data/question_info.txt"
+INVITED_DATA_ADDR = './bytecup2016data/invited_info_train.txt'
+VALIDATION_DATA_ADDR = './bytecup2016data/validate_nolabel.txt'
 
 
 if __name__ == '__main__':
     print "Reading data..."
-    user_data = load_user_data(USER_DATA_ADDR)
-    print user_data[1]
+    user_data = load_data(USER_DATA_ADDR)
+    # question_data = load_data(QUESTION_DATA_ADDR)
+    # invitation_data = load_data(INVITED_DATA_ADDR)
+    # vectorize_user(user_data)
+    print user_data[0]
     
---
+++ parsers.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+def dictionary_generator(data_list):
+    dic_result = {}
+    for item in data_list:
+        pass
+
+
+def vectorize_user(user_data):
+    print user_data[1]
