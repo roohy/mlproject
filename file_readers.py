9909c90faf1aaf8017ff360bed9dc12c1d6a23a5
Negar Golestani <Negar@guest-wireless-upc-1605-10-120-082-235.usc.edu>
2016-10-26 20:11:56 -0700

    load data addedd

--- file_readers.py
+++ file_readers.py
@@ -1,2 +1,8 @@
 import numpy as np
 
+def load_user_data( file_name ):
+    result = []
+    with open(file_name,'r') as user_data:
+        for line in user_data:
+            result.append((line[:-1]).split('\t'))
+    return result
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import numpy as np
+from file_readers import *
+USER_DATA_ADDR = "./bytecup2016data/user_info.txt"
+
 
 if __name__ == '__main__':
-    print "We are starting the fuck up..."
+    print "Reading data..."
+    user_data = load_user_data(USER_DATA_ADDR)
+    print user_data[1]
+    
